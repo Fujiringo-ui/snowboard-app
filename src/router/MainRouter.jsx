df6032941e582e1ac3c75dc8b06b49714e90f318
s1F101901476 <s1f101901476@iniad.org>
2022-11-09 17:11:21 +0900

    fix

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { TopPage, SecondPage } from 'src/Pages/index';
+import { TopPage, SecondPage } from '../Pages';
 import { TestRouter } from '.';
 
 const MainRouter = () => {
