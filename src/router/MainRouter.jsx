d37cd1a86745a6a8cda3b3a0f8c900af1164784a
s1F101901476 <s1f101901476@iniad.org>
2022-11-09 17:09:59 +0900

    fix

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,23 +1,19 @@
-import React from "react";
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes
-} from 'react-router-dom';
-import { TopPage, SecondPage } from "src/Pages";
-import { TestRouter } from ".";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { TopPage, SecondPage } from 'src/Pages/index';
+import { TestRouter } from '.';
 
 const MainRouter = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<TopPage />} />
-                <Route path="/snowpark" element={<SecondPage />} />
-                <Route path="/test/*" element={<TestRouter />} />
-                <Route path="*" element={<div>404 ERROR PAGE</div>} />
-            </Routes>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TopPage />} />
+        <Route path="/snowpark" element={<SecondPage />} />
+        <Route path="/test/*" element={<TestRouter />} />
+        <Route path="*" element={<div>404 ERROR PAGE</div>} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default MainRouter;
